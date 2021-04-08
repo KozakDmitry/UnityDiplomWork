226f6f5b1fec9faff98e4b3cc8166d78297a06e0
Dmitry <dimakaza4ka@gmail.com>
2021-04-08 21:39:02 +0300

    Added start scripts Player and Board Manager

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField]
+    private int[][] board;
+    [SerializeField]
+    private Tilemap map;
+    [SerializeField]
+    private List<Tile> ListOfTiles;
+
+
+
+    private int size = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoardGenerator(size);
+    }
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+    private void BoardGenerator(int size)
+    {
+      
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (this.gameObject.activeSelf)
+        {
+            return;
+        }
+
+        int horizontal = 0, vertical = 0;
+
+        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+        vertical = (int)(Input.GetAxisRaw("Vertical"));
+
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+        
+        if(horizontal!=0 || vertical != 0)
+        {
+            TryToMove(horizontal, vertical);
+        }
+    }
+
+
+
+
+
+    private void TryToMove(int horizontal, int vertical)
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+}
